9ef7ea51b94dec696c90a81aa2c0fe5b7107a7d1
cestari15 <rafacjacobs1502@gmail.com>
2023-08-24 10:42:01 -0300

    Commit Atualizado

---
+++ produto.ts
@@ -0,0 +1,47 @@
+class Produto {
+    nome: string;
+    valor:number;
+    desconto:number;
+
+
+    constructor(nome:string, valor:number, desconto:number)
+    {
+        this.nome=nome;
+        this.valor=valor;
+        this.desconto=desconto;
+    }
+
+    Creatina(){
+
+        
+        
+        return "o produto de nome "+this.nome + " tem o valor de "+ " R$ "+ this.valor + " porém tem " + this.desconto+ " % " + "de desconto "
+        + "terá um novo valor de "+ " R$ "+(this.valor * this.desconto)/100;
+       
+       
+    }
+
+    Whey(){
+        return "o produto de nome "+this.nome + " tem o valor de "+ " R$ "+ this.valor + " porém tem " + this.desconto+ " % " + "de desconto"
+        + "terá um novo valor de "+ " R$ "+(this.valor * this.desconto)/100;
+        
+    }
+
+    Carro(){
+        return "o produto de nome "+this.nome + " tem o valor de "+ " R$ "+ this.valor + " porém tem " + this.desconto+ " % " + "de desconto"
+        + "terá um novo valor de "+ " R$ "+(this.valor * this.desconto)/100;
+    }
+
+    
+}
+
+
+let creatina = new Produto("creatina", 50 , 50);
+let whey = new Produto("Whey",100, 50);
+let carro=new Produto("carro", 50000, 50);
+
+console.log(creatina.Creatina());
+
+console.log(whey.Whey());
+
+console.log(carro.Carro());
