cc53b7cfd1345a57a216910fd4ca360de8edc1d7
cestari15 <rafacjacobs1502@gmail.com>
2023-08-24 15:42:39 -0300

    Atualizando TUDO KKKKKK

---
+++ estudante.ts
@@ -0,0 +1,35 @@
+class Estudante {
+    nome: string;
+    idade: number;
+    notas: number[];
+
+
+    constructor(nome: string, idade: number, notas: number[]) {
+        this.nome = nome;
+        this.idade = idade;
+        this.notas = notas;
+    }
+
+    verificador() {
+        let soma = 0;
+        let media = 0;
+
+        for (let i = 0; i < this.notas.length; i++) {
+            soma = soma + this.notas[i];
+            
+        }
+        media = soma / this.notas.length;
+
+        if (media >= 7) {
+            return "Parabéns," + this.nome + "! Sua média é " + media + " e você está aprovado(a) e você tem " + this.idade + " caso você não saiba";
+        } else {
+            return this.nome, "sua média é " + media + " e você precisa estudar mais para melhorar seu desempenho " + this.idade + " caso você não saiba";
+
+        }
+
+    }
+}
+
+let total = new Estudante("Maria", 20, [8, 7.5, 9.2, 6.8, 9.5]);
+
+console.log(total.verificador());
